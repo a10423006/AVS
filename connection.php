324577619caf5228f87502b228dae40135143aae
anan <A10423006@yuntech.org.tw>
2018-04-12 22:17:15 +0800

    更新

--- connection.php
+++ connection.php
@@ -6,14 +6,6 @@
     // Create connection
     $con = mysqli_connect($servername, $username, $password, $dbname);
 
-    // $servername = "212.1.212.1"; //replace it with your database server name
-    // $username = "bigcattl_test";  //replace it with your database username
-    // $password = "s6951435";  //replace it with your database password
-    // $dbname = "bigcattl_formal";
-    // // Create connection
-    // $conn = mysql_connect($servername, $username, $password);
-    // mysql_select_db("bigcattl_formal", $conn);
-
     mysqli_query("set names utf8");
 
     // Check connection
